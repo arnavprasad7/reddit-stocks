c75c27206a0e89eb885097024c44bb635a8cb6bd
Ronnocsep <74361728+Ronnocsep@users.noreply.github.com>
2020-12-21 00:04:19 +0100

    Pushshift scraping now operational

--- utils.py
+++ utils.py
@@ -67,3 +67,93 @@ def sentiment_score(x):
         score = np.exp(x) - 1
 
     return score
+
+
+
+
+
+
+#Using pushshift
+
+
+import requests
+from datetime import datetime
+import traceback
+import time
+import pandas as pd
+
+
+def download_from_pushshift(output_filename, object_type, chosen_subreddit, date):
+    #Print that code is starting
+    print(f"Saving {object_type}s to {output_filename}")
+    
+    #Template URL for using pushshift
+    url = "https://api.pushshift.io/reddit/{}/search?limit=1000&sort_type=score&sort=desc&subreddit={}&after={}&before={}"
+    
+    subreddit = chosen_subreddit
+
+    #Convert date from string to unix time
+    day = int(time.mktime(datetime.strptime(date, "%Y-%m-%d").timetuple()))
+    
+    count = 0
+    handle = open(output_filename, 'a')
+    
+    #Generate the URL we will use to search reddit
+    formatted_url = url.format(object_type, subreddit, str(day), str(day + 86400)) #set limit to be 24 hours after start time
+    json = requests.get(formatted_url, headers={'User-Agent': "Post downloader by /u/Watchful1"})
+    time.sleep(1) # avoid pushshift rate limit
+    
+    try:
+        json_data = json.json()
+    
+        objects = json_data['data']
+        
+        handle.write("DATE: " + date + "\n\n----------------------\n\n")
+        
+        for object in objects:
+            count += 1
+            if object_type == 'comment':
+                try:
+                    handle.write(str(object['score']))
+                    #handle.write(" : ")
+                    #handle.write(datetime.fromtimestamp(object['created_utc']).strftime("%Y-%m-%d"))
+                    handle.write("\n")
+                    text = object['body']
+                    textASCII = text.encode(encoding='ascii', errors='ignore').decode()
+                    handle.write(textASCII)
+                    handle.write("\n-------------------------------\n")
+                except Exception as err:
+                    print(f"Couldn't print comment: https://www.reddit.com{object['permalink']}")
+                    print(traceback.format_exc())
+            elif object_type == 'submission':
+                try:
+                    handle.write(str(object['score']))
+                    #handle.write(" : ")
+                    #handle.write(datetime.fromtimestamp(object['created_utc']).strftime("%Y-%m-%d"))
+                    handle.write("\n")
+                    text = object['title']
+                    if len(object['selftext']) > 0:
+                        text += " - " 
+                        text += object['selftext']
+                    textASCII = text.encode(encoding='ascii', errors='ignore').decode()
+                    handle.write(textASCII)
+                    handle.write("\n-------------------------------\n")
+                except Exception as err:
+                    print(f"Couldn't print post: {object['url']}")
+                    print(traceback.format_exc())
+        
+        
+        handle.write("\n\n\n\n\n\n\n")
+    except:
+        print("Error: No {}s found on {}".format(object_type, date))
+    
+    
+    print("Saved {} {}s from {}".format(count, object_type, datetime.fromtimestamp(day).strftime("%Y-%m-%d")))
+    handle.close()
+   
+
+
+
+
+
+
